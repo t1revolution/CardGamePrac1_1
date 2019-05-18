0cfad802353f9bc3d1ed09f3b7e2669125e89e77
t1revolution <ichinenndettt@ymobile.ne.jp>
2019-05-19 08:14:30 +0900

    初期状態

---
+++ Assets/DragObj.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragObj : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentTransform;
+    public void OnBeginDrag(PointerEventData data)
+    {
+        Debug.Log("OnBeginDrag");
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        parentTransform = transform.parent;
+        transform.SetParent(transform.parent.parent);
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        transform.position = data.position;
+    }
+    public void OnEndDrag(PointerEventData data)
+    {
+        Debug.Log("OnEndDrag");
+        transform.SetParent(parentTransform);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
+
+//
---
+++ Assets/DropArea.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropArea : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData data)
+    {
+        Debug.Log(gameObject.name);
+
+        DragObj dragObj = data.pointerDrag.GetComponent<DragObj>();
+        if (dragObj != null)
+        {
+            dragObj.parentTransform = this.transform;
+            Debug.Log(gameObject.name + "に" + data.pointerDrag.name + "をドロップ");
+        }
+    }
+}
